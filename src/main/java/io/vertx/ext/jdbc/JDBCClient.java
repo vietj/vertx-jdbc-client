f64d12d4137c7715f1db4bae402219ef0adbc81d
Paulo Lopes <paulo@mlopes.net>
2018-03-29 21:30:11 +0200

    test swapping the cp

--- src/main/java/io/vertx/ext/jdbc/JDBCClient.java
+++ src/main/java/io/vertx/ext/jdbc/JDBCClient.java
@@ -38,7 +38,7 @@ public interface JDBCClient extends SQLClient {
   /**
    * The default data source provider is C3P0
    */
-  String DEFAULT_PROVIDER_CLASS =  "io.vertx.ext.jdbc.spi.impl.C3P0DataSourceProvider";
+  String DEFAULT_PROVIDER_CLASS =  "io.vertx.ext.jdbc.spi.impl.AgroalCPDataSourceProvider";
 
   /**
    * The name of the default data source
